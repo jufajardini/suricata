218ab2b0697aac3be7fd4f3e5616a71d6063f61d
Juliana Fajardini <jufajardini@gmail.com>
2025-02-28 19:18:47 -0300

    flow/output: log triggered exception policies
    To accompany the Exception Policy stats, also add information about any
    Exception Policy triggered and for which target to the flow log event.
    
    Task #6215

--- src/output-json-flow.c
+++ src/output-json-flow.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2007-2020 Open Information Security Foundation
+/* Copyright (C) 2007-2025 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -50,6 +50,7 @@
 #include "stream-tcp.h"
 #include "stream-tcp-private.h"
 #include "flow-storage.h"
+#include "util-exception-policy.h"
 
 static JsonBuilder *CreateEveHeaderFromFlow(const Flow *f)
 {
@@ -279,6 +280,21 @@ static void EveFlowLogJSON(OutputJsonThreadCtx *aft, JsonBuilder *jb, Flow *f)
         JB_SET_STRING(jb, "action", "drop");
     } else if (f->flags & FLOW_ACTION_PASS) {
         JB_SET_STRING(jb, "action", "pass");
+    }
+    if (f->flags & FLOW_TRIGGERED_EXCEPTION_POLICY) {
+        jb_open_array(jb, "exception_policy");
+        // if the exception policy target is 0, we won't log, so start from 1
+        for (uint16_t i = 1; i < EXCEPTION_POLICY_TARGETS_MAX; i++) {
+            uint16_t tmp_flag = GetTargetFlagFromTargetEnum((enum ExceptionPolicyTargetValues)i);
+            if (f->applied_exception_policy & tmp_flag) {
+                jb_start_object(jb);
+                jb_set_string(jb, "target", ExceptionPolicyTargetFlagToString(tmp_flag));
+                jb_set_string(jb, "policy",
+                        ExceptionPolicyEnumToString(ExceptionPolicyTargetPolicy(tmp_flag), true));
+                jb_close(jb);
+            }
+        }
+        jb_close(jb); /* close array */
     }
 
     /* Close flow. */
